4f301f5037dc04f31ac6573d3dcd1d5210155083
ThousandAITech <thousand.ai.tech@gmail.com>
2023-07-02 22:29:16 +0800

    1. user app 2. data app 3. MyUser, UserProfile

---
+++ ltserChanghua/user/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import MyUser, UserProfile
+from django import forms
+class MyUserForm(forms.ModelForm):
+    class Meta:
+        model = MyUser
+        exclude = ['password']
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    extra = 1
+class MyUserAdmin(admin.ModelAdmin):
+    form = MyUserForm
+    list_display = ('id', 'get_email', 'get_name', 'get_verified', 'get_last_login')
+    inlines = [UserProfileInline]
+    readonly_fields = ('email', 'username')
+    def get_email(self, obj):
+        return obj.email
+
+    def get_verified(self, obj):
+        return obj.is_verified
+
+    def get_name(self, obj):
+        return obj.last_name + obj.first_name
+
+    def get_last_login(self, obj):
+        return obj.last_login
+
+    get_email.short_description = 'EMAIL ADDRESS'
+    get_verified.short_description = 'VERIFIED STATUS'
+    get_name.short_description = 'NAME'
+    get_last_login.short_description = 'LAST LOGIN'
+
+admin.site.register(MyUser, MyUserAdmin)
---
+++ ltserChanghua/user/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils import timezone
+class MyUserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        if username is None:
+            raise TypeError('創建使用者必須輸入 username')
+        if email is None:
+            raise TypeError('創建使用者必須輸入 email')
+
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('創建管理員必須輸入 password')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_verified = True
+        user.is_staff = True
+        user.is_applied = True
+        user.save()
+        return user
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_applied = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    last_login = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+    def update_last_login(self):
+        self.last_login = timezone.now()
+        self.save(update_fields=['last_login'])
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    school = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    location = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    department = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    title = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    category = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    application = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    attention = models.CharField(max_length=100, null=True, blank=True, editable=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
