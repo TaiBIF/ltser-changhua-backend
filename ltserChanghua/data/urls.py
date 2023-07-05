93e7d1487656702cc30b0a7ab55c304bd781c0f8
ThousandAITech <thousand.ai.tech@gmail.com>
2023-07-05 22:34:37 +0800

    changeLatestEventViews api

--- ltserChanghua/data/urls.py
+++ ltserChanghua/data/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomepagePhotoAPIView, LatestEventAPIView
+from .views import HomepagePhotoAPIView, LatestEventAPIView, ChangeLatestEventViewsAPIView
 
 urlpatterns = [
     path("getHomepagePhotos", HomepagePhotoAPIView.as_view(), name="getHomepagePhotos"),
     path("getLatestEvents", LatestEventAPIView.as_view(), name="getLatestEvents"),
+    path("changeLatestEventViews/<str:pk>", ChangeLatestEventViewsAPIView.as_view(), name="changeLatestEventViews"),
 ]
