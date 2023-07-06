436d5479272084ca91155bddf6a221809a059eb0
ThousandAITech <thousand.ai.tech@gmail.com>
2023-07-06 23:58:22 +0800

    ResendVerifyEmail

--- ltserChanghua/user/urls.py
+++ ltserChanghua/user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegisterAPIView, VerifyEmailAPIView
+from .views import RegisterAPIView, VerifyEmailAPIView, ResendEmailVerifyAPIView
 
 urlpatterns = [
     path('signUp/', RegisterAPIView.as_view(), name='sign-up'),
     path('email-verify/', VerifyEmailAPIView.as_view(), name='email-verify'),
-    path('resend-email-verify/', views.ResendEmailVerify.as_view(), name='resend-email-verify'),
+    path('resend-email-verify/', ResendEmailVerifyAPIView.as_view(), name='resend-email-verify'),
 ]
