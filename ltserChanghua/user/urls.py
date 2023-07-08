f961faaffad9125f43de41fa3a4ee7a21b0900af
ThousandAITech <thousand.ai.tech@gmail.com>
2023-07-08 11:52:01 +0800

    login/refresh

--- ltserChanghua/user/urls.py
+++ ltserChanghua/user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import RegisterAPIView, VerifyEmailAPIView, ResendEmailVerifyAPIView, LoginAPIView, UserProfileAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('signUp/', RegisterAPIView.as_view(), name='sign-up'),
     path('email-verify/', VerifyEmailAPIView.as_view(), name='email-verify'),
     path('resend-email-verify/', ResendEmailVerifyAPIView.as_view(), name='resend-email-verify'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('getUserProfile/', UserProfileAPIView.as_view(), name='getUserProfile'),
 ]
