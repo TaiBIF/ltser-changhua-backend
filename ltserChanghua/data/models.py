e392c1e4d5a3157af8383731e9a4f8f619bb174b
ThousandAITech <thousand.ai.tech@gmail.com>
2023-08-22 22:36:45 +0800

    change Crab Site latitude and longitude

--- ltserChanghua/data/models.py
+++ ltserChanghua/data/models.py
@@ -39,8 +39,6 @@ class CrabSite(models.Model):
     title = models.CharField(max_length=200)
     latitude = models.FloatField(null=True, blank=True)
     longitude =  models.FloatField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"螃蟹觀測點位: {self.title}"
     class Meta:
