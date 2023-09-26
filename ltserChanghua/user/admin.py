1a046a81146a1e3c788e0ad7a3d6fd75126c8d05
ThousandAITech <thousand.ai.tech@gmail.com>
2023-09-26 11:52:37 +0800

    update myuser admin interface

--- ltserChanghua/user/admin.py
+++ ltserChanghua/user/admin.py
@@ -12,12 +12,18 @@ class MyUserForm(forms.ModelForm):
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     extra = 1
+
+
 class MyUserAdmin(admin.ModelAdmin):
     form = MyUserForm
-    list_display = ('id', 'get_email', 'get_name', 'get_verified', 'get_last_login')
+    list_display = (
+    'id', 'get_email', 'get_name', 'get_verified', 'get_last_login', 'get_school', 'get_location', 'get_department',
+    'get_title', 'get_category', 'get_application', 'get_attention')
     inlines = [UserProfileInline]
     readonly_fields = ('email', 'username')
     search_fields = ['email']
+    actions = ['export_as_csv']
+
     def get_email(self, obj):
         return obj.email
 
@@ -30,10 +36,84 @@ class MyUserAdmin(admin.ModelAdmin):
     def get_last_login(self, obj):
         return obj.last_login
 
-    get_email.short_description = 'EMAIL ADDRESS'
-    get_verified.short_description = 'VERIFIED STATUS'
-    get_name.short_description = 'NAME'
-    get_last_login.short_description = 'LAST LOGIN'
+    def get_school(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.school
+        return ''
+
+    def get_location(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.location
+        return ''
+
+    def get_department(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.department
+        return ''
+
+    def get_title(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.title
+        return ''
+
+    def get_category(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.category
+        return ''
+
+    def get_application(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.application
+        return ''
+
+    def get_attention(self, obj):
+        if hasattr(obj, 'userprofile'):
+            return obj.userprofile.attention
+        return ''
+
+    def export_as_csv(self, request, queryset):
+        fields = ['id', 'email', 'get_name', 'is_verified', 'last_login',
+                  'school', 'location', 'department', 'title', 'category',
+                  'application', 'attention']
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="user.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Email', 'Name', 'Is Verified', 'Last Login',
+                         'School', 'Location', 'Department', 'Title', 'Category',
+                         'Application', 'Attention'])
+
+        for obj in queryset:
+            row = []
+            for field in fields:
+                if field in ['school', 'location', 'department', 'title',
+                             'category', 'application', 'attention']:
+                    if hasattr(obj, 'userprofile'):
+                        row.append(getattr(obj.userprofile, field) or '')
+                    else:
+                        row.append('')
+                elif hasattr(obj, field):
+                    row.append(getattr(obj, field) or '')
+                elif hasattr(self, field):  # 检查是否是方法
+                    row.append(getattr(self, field)(obj) or '')
+                else:
+                    row.append('')
+            writer.writerow(row)
+        return response
+
+    export_as_csv.short_description = "輸出會員資料"
+    get_email.short_description = 'email'
+    get_verified.short_description = 'verified'
+    get_name.short_description = 'name'
+    get_last_login.short_description = 'last login'
+    get_school.short_description = 'school'
+    get_location.short_description = 'location'
+    get_department.short_description = 'department'
+    get_title.short_description = 'title'
+    get_category.short_description = 'category'
+    get_application.short_description = 'application'
+    get_attention.short_description = 'attention'
 
 
 class DownloadRecordAdmin(admin.ModelAdmin):
